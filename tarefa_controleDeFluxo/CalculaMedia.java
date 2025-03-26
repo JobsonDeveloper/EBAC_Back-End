4a2f2fb9776c1a88baec32ca71f9bce9deeee04c
Jobson Oliveira <jobsondeveloper@gmail.com>
2025-03-26 19:51:43 -0300

    Adicionando atividade

--- tarefa_controleDeFluxo/CalculaMedia.java
+++ tarefa_controleDeFluxo/CalculaMedia.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CalculaMedia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Double somaNotas = 0.0;
+        Integer media = 7;
+        Double notaFinal;
+
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("Digite a nota da " + i + "ª prova: ");
+            somaNotas += scan.nextDouble();
+        }
+
+        notaFinal = somaNotas / 4;
+
+        if (notaFinal >= 7) {
+            System.out.println("Você foi aprovado com a média " + notaFinal);
+        }
+        else if(notaFinal >= 5) {
+            System.out.println("Você ficou em recuperação com a média " + notaFinal);
+        }
+        else {
+            System.out.println("Você foi reprovado com a nota " + notaFinal);
+        }
+    }
 }
